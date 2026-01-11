5fb9435807be9142a540a764bc8165f76fbb4149
Nikita <nikita221006@mail.ru>
2026-01-11 15:05:01 +0500

    Ready for deploy

--- nhlStats/settings.py
+++ nhlStats/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-x1)kd55%q&jzw66&p4#@m04&8*n2^(rh8wl)1bd=bku0h81w@q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zikitos888.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -116,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
